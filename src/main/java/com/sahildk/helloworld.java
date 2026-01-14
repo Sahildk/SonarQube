87cdec7819e68fd97b3a91f59656f42dc7a11ec4
sahil <sahildeore1234@gmail.com>
2026-01-14 09:42:35 +0530

    Update helloworld.java

--- src/main/java/com/sahildk/helloworld.java
+++ src/main/java/com/sahildk/helloworld.java
@@ -1,5 +1,7 @@
-public class helloworld { 
-	public static void main(String[] args){ 
-		System.out.println("Hello,World"); 
-	} 
+package com.sahildk;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello SonarCloud");
+    }
 }
